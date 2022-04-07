267569ea6fdd18555172ccc8adea3962d2d0a0c6
sikozsolt96 <sikozsolt96@gmail.com>
2022-04-07 22:36:35 +0300

    nav bar, new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LoginComponent,
+    RegisterComponent,
+    RecipeListComponent
   ],
   imports: [
     BrowserModule
